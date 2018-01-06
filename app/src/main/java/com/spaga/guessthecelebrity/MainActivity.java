4068e4ea39d03e131c01adce16f78dab7aacf125
spagabtti <spagabtti@gmail.com>
2018-01-06 10:05:28 -0800

    edit

--- app/src/main/java/com/spaga/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/spaga/guessthecelebrity/MainActivity.java
@@ -1,21 +1,47 @@
 package com.spaga.guessthecelebrity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.ExecutionException;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> celebImages;
+    ArrayList<String> celebNames;
+    int chosenCeleb;
+    ImageView imageView;
+
+    public class ImageDownloader extends  AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
@@ -40,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            return null;
+            return result;
         }
     }
 
@@ -53,6 +79,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        celebImages = new ArrayList<>();
+        celebNames = new ArrayList<>();
+        imageView = findViewById(R.id.imageView);
         DownloadTask task = new DownloadTask();
         String result = null;
 
@@ -63,16 +92,24 @@ public class MainActivity extends AppCompatActivity {
             Pattern p = Pattern.compile("src=\"(.*?)\"");
             Matcher m = p.matcher(splitResult[0]);
             while (m.find()) {
-                System.out.println(m.group(1));
+                celebImages.add(m.group(1));
             }
 
             p = Pattern.compile("alt=\"(.*?)\"");
             m = p.matcher(splitResult[0]);
             while (m.find()) {
-                System.out.println(m.group(1));
+                celebNames.add(m.group(1));
             }
 
-            Log.i("Contents of URL: ", result);
+            Random random = new Random();
+            chosenCeleb = random.nextInt(celebImages.size());
+
+            ImageDownloader imageTask = new ImageDownloader();
+            Bitmap bitmap;
+
+            bitmap = imageTask.execute(celebImages.get(chosenCeleb)).get();
+            imageView.setImageBitmap(bitmap);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
